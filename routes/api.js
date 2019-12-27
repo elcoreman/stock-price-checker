ff014eef509df81d84cffacba9a1c8602a5fecf5
Glitch (ecr-stock-price-checker) <none>
2019-12-27 19:37:05 +0000

    📼🚏 Checkpoint ./routes/api.js:9298160/6993

--- routes/api.js
+++ routes/api.js
@@ -7,12 +7,12 @@ const request = require("request");
 const CONNECTION_STRING = process.env.DB;
 
 module.exports = app => {
-  const stocks = (res, result, like, d1, d2) => {
+  const stocks = (res, result, like, ip, d1, d2) => {
     MongoClient.connect(CONNECTION_STRING, (err, client) => {
       assert.equal(null, err);
       let col = client.db("test").col("stocks_ip");
       col.findOneAndUpdate(
-        { symbol: d1.symbol.toLowerCase() },
+        { symbol: d1.symbol.toLowerCase(), ips: { $nin: [ip] } },
         like ? { $set: { d: 1 } } : {},
         {},
         (err, dbResult) => {
@@ -29,6 +29,7 @@ module.exports = app => {
       : req.query.stock;
     let like = req.query.like ? req.query.like.toLowerCase() === "true" : false;
     let result = { stockData: { likes: 0 } };
+    const ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
     if (stock == []) return res.json(result);
     request(
       "https://repeated-alpaca.glitch.me/v1/stock/" + stock[0] + "/quote",
@@ -41,12 +42,12 @@ module.exports = app => {
                 "/quote",
               (error, response, body2) => {
                 if (!error && response.statusCode == 200) {
-                  stocks(res, result, like, body1, body2);
+                  stocks(res, result, like, ip, body1, body2);
                 }
               }
             );
           } else {
-            stocks(res, result, like, body1);
+            stocks(res, result, like, ip, body1);
           }
         }
       }
