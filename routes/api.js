f8a7e302fbd43570b7947bde9ad94d73c5b35012
Glitch (ecr-stock-price-checker) <none>
2019-12-27 20:12:05 +0000

    🏢🌵 Checkpoint ./routes/api.js:9298160/13303

--- routes/api.js
+++ routes/api.js
@@ -8,60 +8,67 @@ const CONNECTION_STRING = process.env.DB;
 
 module.exports = app => {
   const stocks = (res, result, like, ip, d1, d2) => {
-    MongoClient.connect(CONNECTION_STRING, (err, client) => {
-      assert.equal(null, err);
-      let col = client.db("test").col("stocks_ip");
-      let q = {};
-      q.symbol = d1.symbol.toLowerCase();
-      q.ips = { $nin: [ip] };
-      col.findOneAndUpdate(
-        q,
-        like ? { $push: { ips: ip } } : {},
-        {},
-        (err, dbResult) => {
-          assert.equal(null, err);
-          col.findOne({ symbol: q.symbol }, (err, dbResult) => {
+    console.log(d1.symbol);
+    MongoClient.connect(
+      CONNECTION_STRING,
+      { useUnifiedTopology: true },
+      (err, client) => {
+        console.log(d1.symbol);
+        assert.equal(null, err);
+        let col = client.db("test").collection("stocks_ip");
+        let q = {};
+        console.log(d1.symbol);
+        q.symbol = d1.symbol.toLowerCase();
+        q.ips = { $nin: [ip] };
+        col.findOneAndUpdate(
+          q,
+          like ? { $push: { ips: ip } } : {},
+          {returnOriginal: false},
+          (err, dbResult) => {
             assert.equal(null, err);
-            let likesCount1 = dbResult.ips.length;
-            let res1 = {
-              stock: d1.symbol,
-              price: d1.latestPrice,
-              likes: likesCount1
-            };
-            if (!d2) {
-              res.json({
-                stockData: res1
-              });
-            } else {
-              q.symbol = d2.symbol.toLowerCase();
-              q.ips = { $nin: [ip] };
-              col.findOneAndUpdate(
-                q,
-                like ? { $push: { ips: ip } } : {},
-                {},
-                (err, dbResult) => {
-                  assert.equal(null, err);
-                  col.findOne({ symbol: q.symbol }, (err, dbResult) => {
+            col.findOne({ symbol: q.symbol }, (err, dbResult) => {
+              assert.equal(null, err);
+              let likesCount1 = dbResult.ips.length;
+              let res1 = {
+                stock: d1.symbol,
+                price: d1.latestPrice,
+                likes: likesCount1
+              };
+              if (!d2) {
+                res.json({
+                  stockData: res1
+                });
+              } else {
+                q.symbol = d2.symbol.toLowerCase();
+                q.ips = { $nin: [ip] };
+                col.findOneAndUpdate(
+                  q,
+                  like ? { $push: { ips: ip } } : {},
+                  {},
+                  (err, dbResult) => {
                     assert.equal(null, err);
-                    let likesCount2 = dbResult.ips.length;
-                    let res2 = {
-                      stock: d2.symbol,
-                      price: d2.latestPrice,
-                      rel_likes: likesCount2-likesCount2
-                    };
-                    delete res1.likes;
-                    
-                    res.json({
-                      stockData: [res1, res2]
+                    col.findOne({ symbol: q.symbol }, (err, dbResult) => {
+                      assert.equal(null, err);
+                      let likesCount2 = dbResult.ips.length;
+                      let res2 = {
+                        stock: d2.symbol,
+                        price: d2.latestPrice,
+                        rel_likes: likesCount2 - likesCount1
+                      };
+                      delete res1.likes;
+                      res2.rel_likes = likesCount1 - likesCount2;
+                      res.json({
+                        stockData: [res1, res2]
+                      });
                     });
-                  });
-                }
-              );
-            }
-          });
-        }
-      );
-    });
+                  }
+                );
+              }
+            });
+          }
+        );
+      }
+    );
   };
 
   app.route("/api/stock-prices").get((req, res) => {
@@ -77,6 +84,7 @@ module.exports = app => {
       "https://repeated-alpaca.glitch.me/v1/stock/" + stock[0] + "/quote",
       (error, response, body1) => {
         if (!error && response.statusCode == 200) {
+          body1 = JSON.parse(body1);
           if (stock[1]) {
             request(
               "https://repeated-alpaca.glitch.me/v1/stock/" +
@@ -84,6 +92,7 @@ module.exports = app => {
                 "/quote",
               (error, response, body2) => {
                 if (!error && response.statusCode == 200) {
+                  body2 = JSON.parse(body2);
                   stocks(res, result, like, ip, body1, body2);
                 }
               }
