3a6b581956a57a848c2bbc69772eb2ed5292cafe
Glitch (ecr-stock-price-checker) <none>
2019-12-27 16:32:05 +0000

    📫👀 Checkpoint ./routes/api.js:9298160/43

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,8 @@ var MongoClient = require("mongodb");
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function(app) {
-  app.route("/api/stock-prices").get(function(req, res) {});
+  app.route("/api/stock-prices").get(function(req, res) {
+    
+    
+  });
 };
