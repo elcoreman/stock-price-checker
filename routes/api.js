d515b176bcaa73e35aa7d1814d87ebb5ef08c1e0
Glitch (ecr-stock-price-checker) <none>
2019-12-27 16:44:03 +0000

    👫🚘 Checkpoint ./routes/api.js:9298160/1389 ./tests/2_functional-tests.js:9298160/181

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,17 @@
 "use strict";
 
-var expect = require("chai").expect;
 var MongoClient = require("mongodb");
+var assert = require('chai').assert;
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = process.env.DB;
 
-module.exports = function(app) {
-  app.route("/api/stock-prices").get(function(req, res) {
-    
-    
+module.exports = (app)=> {
+  app.route("/api/stock-prices").get((req, res)=> {
+    let stock = req.query.stock;
+    let like = req.query.like.toLowerCase() === "true";
+    MongoClient.connect(CONNECTION_STRING, (err, client)=> {
+      assert.equal(null, err);
+      let col = client.db("test").col(stock);
+    });
   });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,3 @@
-/*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
-
 var chaiHttp = require('chai-http');
 var chai = require('chai');
 var assert = chai.assert;
