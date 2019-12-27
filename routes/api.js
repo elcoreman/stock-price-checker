fbec08f577f3cbec956123433f7bf91d90e151c9
Glitch (ecr-stock-price-checker) <none>
2019-12-27 18:50:08 +0000

    🐮👼 Checkpoint ./routes/api.js:9298160/38323

--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,17 @@
 
 var MongoClient = require("mongodb");
 var assert = require("chai").assert;
-const get = require("https").get;
+const https = require("https");
 
 const CONNECTION_STRING = process.env.DB;
 
 module.exports = app => {
   const fn = (d1, d2) => {
-    console.log(d1, d2);
+    d1 = process.stdout.write(d1);
+    d2 = process.stdout.write(d2);
+    //console.log(d1);
   };
+
 
   app.route("/api/stock-prices").get((req, res) => {
     let stock = [];
@@ -17,29 +20,33 @@ module.exports = app => {
       ? stock.push(req.query.stock)
       : req.query.stock;
     let like = req.query.like ? req.query.like.toLowerCase() === "true" : false;
-    get(
-      "https://repeated-alpaca.glitch.me/v1/stock/" + stock[0] + "/quote",
-      res =>
-        res.on("data", data1 => {
-          if (stock[1]) {
-            get(
-              "https://repeated-alpaca.glitch.me/v1/stock/" +
-                stock[1] +
-                "/quote",
-              res =>
-                res.on("data", data2 => {
-                  fn(data1, data2);
-                })
-            ).on("error", err => {
-              console.error(err);
-            });
-          } else {
-            fn(data1);
-          }
-        })
-    ).on("error", err => {
-      console.error(err);
-    });
+    https
+      .get(
+        "https://repeated-alpaca.glitch.me/v1/stock/" + stock[0] + "/quote",
+        res =>
+          res.on("data", data1 => {
+            if (stock[1]) {
+              https
+                .get(
+                  "https://repeated-alpaca.glitch.me/v1/stock/" +
+                    stock[1] +
+                    "/quote",
+                  res =>
+                    res.on("data", data2 => {
+                      fn(data1, data2);
+                    })
+                )
+                .on("error", err => {
+                  console.error(err);
+                });
+            } else {
+              fn(data1);
+            }
+          })
+      )
+      .on("error", err => {
+        console.error(err);
+      });
 
     /*MongoClient.connect(CONNECTION_STRING, (err, client) => {
       assert.equal(null, err);
