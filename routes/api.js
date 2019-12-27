ed5ca997636ecc6c8d1aba82f54c245c98201541
Glitch (ecr-stock-price-checker) <none>
2019-12-27 17:07:04 +0000

    🍰🐚 Checkpoint ./routes/api.js:9298160/10319 ./package.json:9298160/1322

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,37 @@
 "use strict";
 
 var MongoClient = require("mongodb");
-var assert = require('chai').assert;
-const https = require('https');
+var assert = require("chai").assert;
+const https = require("https");
 
 const CONNECTION_STRING = process.env.DB;
 
-module.exports = (app)=> {
-  app.route("/api/stock-prices").get((req, res)=> {
+const getStock = stockName => {
+  https
+    .request(
+      {
+        hostname:
+          "https://repeated-alpaca.glitch.me/v1/stock/" + stockName + "/quote",
+        method: "GET"
+      },
+      res => res.on("data", d => d)
+    )
+    .on("error", error => {
+      console.error(error);
+    })
+    .end();
+};
+module.exports = app => {
+  app.route("/api/stock-prices").get((req, res) => {
     let stock = req.query.stock;
     let like = req.query.like.toLowerCase() === "true";
-    MongoClient.connect(CONNECTION_STRING, (err, client)=> {
+    
+    console.log(getStock("goog"));
+    
+    /*MongoClient.connect(CONNECTION_STRING, (err, client) => {
       assert.equal(null, err);
       let col = client.db("test").col("stocksip");
-      
-    });
+    });*/
+    
   });
 };
