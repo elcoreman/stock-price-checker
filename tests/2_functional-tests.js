23ebb54f5a667a83383128a53fd64bab397a16f4
Glitch (ecr-stock-price-checker) <none>
2019-12-30 11:12:50 +0000

    🍣💓 Checkpoint ./tests/2_functional-tests.js:9298160/9485

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,42 +1,59 @@
-var chaiHttp = require('chai-http');
-var chai = require('chai');
+var chaiHttp = require("chai-http");
+var chai = require("chai");
 var assert = chai.assert;
-var server = require('../server');
+var server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-    
-    suite('GET /api/stock-prices => stockData object', function() {
-      
-      test('1 stock', function(done) {
-       chai.request(server)
-        .get('/api/stock-prices')
-        .query({stock: 'goog'})
-        .end(function(err, res){
-          
-          
-          
+let likes;
+
+suite("Functional Tests", function() {
+  suite("GET /api/stock-prices => stockData object", function() {
+    test("1 stock", function(done) {
+      chai
+        .request(server)
+        .get("/api/stock-prices")
+        .query({ stock: "goog" })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.property(res.body, "stockData");
+          assert.isObject(res.body.stockData);
+          assert.property(res.body.stockData, "stock");
+          assert.property(res.body.stockData, "price");
+          assert.property(res.body.stockData, "likes");
+          assert.isString(res.body.stockData.stock);
+          assert.isNumber(res.body.stockData.price);
+          assert.isNumber(res.body.stockData.likes);
           done();
         });
-      });
-      
-      test('1 stock with like', function(done) {
-        
-      });
-      
-      test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
-      });
-      
-      test('2 stocks', function(done) {
-        
-      });
-      
-      test('2 stocks with like', function(done) {
-        
-      });
-      
     });
 
+    test("1 stock with like", function(done) {
+      chai
+        .request(server)
+        .get("/api/stock-prices")
+        .query({ stock: "goog" })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.property(res.body, "stockData");
+          assert.isObject(res.body.stockData);
+          assert.property(res.body.stockData, "stock");
+          assert.property(res.body.stockData, "price");
+          assert.property(res.body.stockData, "likes");
+          assert.isString(res.body.stockData.stock);
+          assert.isNumber(res.body.stockData.price);
+          assert.isNumber(res.body.stockData.likes);
+          likes = res.body.stockData.likes;
+          done();
+        });
+    });
+
+    test("1 stock with like again (ensure likes arent double counted)", function(done) {});
+
+    test("2 stocks", function(done) {});
+
+    test("2 stocks with like", function(done) {});
+  });
 });
